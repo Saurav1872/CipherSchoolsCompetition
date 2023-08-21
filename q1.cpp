2ac5a558f66052a9c98c1089dc8492ec8952a8dd
Saurav1872 <saurav.suman1807@gmail.com>
2023-08-22 01:17:18 +0530

    uploaded day 1 soln

--- q1.cpp
+++ q1.cpp
@@ -23,9 +23,23 @@ int maxWater(int heights[], int size) {
 int main() {
     // test case 1
     int heights1[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-    cout << maxWater(heights1, 9) << endl;
+    cout << "Test Case 1" << endl << "Input:" << endl;
+    for (int i = 0; i < 9; i++)
+    {
+        cout << heights1[i] << " ";
+    }
+    cout << endl << "Output:" << endl;
+    
+    cout << maxWater(heights1, 9) << endl << endl;
 
     // test case 2
     int heights2[] = {1, 1};
+
+    cout << "Test Case 2" << endl << "Input:" << endl;
+    for (int i = 0; i < 2; i++)
+    {
+        cout << heights2[i] << " ";
+    }
+    cout << endl << "Output:" << endl;
     cout << maxWater(heights2, 2) << endl;
 }
