b608394cd0af1f8a67f1cb45471857f7f8134730
Saurav1872 <saurav.suman1807@gmail.com>
2023-09-12 01:03:34 +0530

    Uploded day2-v2 soln

---
+++ v2/day1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Node
+{
+    int val;
+    Node *next;
+    Node(int x) : val(x), next(nullptr) {}
+};
+
+int pairSum(Node *head)
+{
+    int res = 0;
+    vector<int> arr;
+    while (head->next != NULL)
+    {
+        arr.push_back(head->val);
+        head = head->next;
+    }
+    arr.push_back(head->val);
+    for (int i = 0; i < (int)arr.size() / 2; i++)
+    {
+        res = max(res, arr[i] + arr[(int)arr.size() - i - 1]);
+    }
+    return res;
+}
+
+void append(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (!head)
+    {
+        head = newNode;
+    }
+    else
+    {
+        Node *current = head;
+        while (current->next)
+        {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+void printLinkedList(Node *head)
+{
+    Node *current = head;
+    while (current)
+    {
+        cout << current->val << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    cout << "Input: " << endl;
+    int n;
+    cin >> n;
+
+    Node *head = nullptr;
+    for (int i = 0; i < n; ++i)
+    {
+        int val;
+        cin >> val;
+        append(head, val);
+    }
+
+    cout << endl << endl << "Output: " << endl;
+    
+    cout << pairSum(head) << endl;
+    return 0;
+}
