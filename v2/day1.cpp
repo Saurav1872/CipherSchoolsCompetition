8c05a1fb1bd9f2fa034b8363c6390a8221e6f723
Saurav1872 <saurav.suman1807@gmail.com>
2023-09-12 01:06:53 +0530

    Uploded day2-v2 soln

--- v2/day1.cpp
+++ v2/day1.cpp
@@ -1,76 +1,58 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-struct Node
-{
+struct Node {
     int val;
     Node *next;
     Node(int x) : val(x), next(nullptr) {}
 };
-
-int pairSum(Node *head)
-{
+int pairSum(Node *head) {
     int res = 0;
     vector<int> arr;
-    while (head->next != NULL)
-    {
+    while (head->next != NULL) {
         arr.push_back(head->val);
         head = head->next;
     }
     arr.push_back(head->val);
-    for (int i = 0; i < (int)arr.size() / 2; i++)
-    {
+    for (int i = 0; i < (int)arr.size() / 2; i++) {
         res = max(res, arr[i] + arr[(int)arr.size() - i - 1]);
     }
     return res;
 }
-
-void append(Node *&head, int val)
-{
+void append(Node *&head, int val) {
     Node *newNode = new Node(val);
-    if (!head)
-    {
+    if (!head) {
         head = newNode;
     }
-    else
-    {
+    else {
         Node *current = head;
-        while (current->next)
-        {
+        while (current->next) {
             current = current->next;
         }
         current->next = newNode;
     }
 }
-
 void printLinkedList(Node *head)
 {
     Node *current = head;
-    while (current)
-    {
+    while (current) {
         cout << current->val << " ";
         current = current->next;
     }
     cout << endl;
 }
-
-int main()
-{
+int main() {
     cout << "Input: " << endl;
     int n;
     cin >> n;
 
     Node *head = nullptr;
-    for (int i = 0; i < n; ++i)
-    {
+    for (int i = 0; i < n; ++i) {
         int val;
         cin >> val;
         append(head, val);
     }
-
-    cout << endl << endl << "Output: " << endl;
-    
+    cout << "Output: " << endl;
     cout << pairSum(head) << endl;
     return 0;
 }
